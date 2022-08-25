a3094c64eb0e0846d568bfe4e2d5090b17800a17
Nikhil Prasad <nikhilprasad1999@gmail.com>
2022-08-25 14:35:44 +0530

    Added Eureka server and API Gateway to repository

---
+++ API-Gateway/src/main/java/com/capg/APIGateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.capg.APIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
