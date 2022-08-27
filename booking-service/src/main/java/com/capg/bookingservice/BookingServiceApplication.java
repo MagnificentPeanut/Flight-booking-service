479e87b2d3b93bceaa7eb79483e0d46c08b40111
Nikhil Prasad <nikhilprasad1999@gmail.com>
2022-08-27 18:32:53 +0530

    Added hystrix dependency to pom.xml and RestTemplate bean to main class.

--- booking-service/src/main/java/com/capg/bookingservice/BookingServiceApplication.java
+++ booking-service/src/main/java/com/capg/bookingservice/BookingServiceApplication.java
@@ -2,9 +2,14 @@ package com.capg.bookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.capg.entity")
@@ -12,9 +17,17 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @ComponentScan(basePackages = "com.capg.service")
 @ComponentScan(basePackages = "com.capg.dto")
 @EnableMongoRepositories("com.capg.repository")
+@EnableCircuitBreaker
 @EnableEurekaClient
 public class BookingServiceApplication {
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(3000);
+		return new RestTemplate(clientHttpRequestFactory);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BookingServiceApplication.class, args);
 	}
